6d5153e208c5d0a5145f336b7dbe7173e03b3cad
Jalen Lewis <jalenlew1995@gmail.com>
2020-11-27 01:17:31 -0600

    Updated db.js

--- public/db.js
+++ public/db.js
@@ -19,3 +19,44 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log("Uh Oh!" + event.target.errorCode);
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    // Delete the records if successful
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+function deletePending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
+}
+
+// Listen for the app to come back online
+window.addEventListener("online", checkDatabase);
